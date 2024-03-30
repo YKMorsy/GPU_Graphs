dc821010e7ddd5f0197825fc8bd363c651826c2e
YKMorsy <ykm3@pitt.edu>
2024-03-30 02:26:38 -0400

    include .h removed

--- include/gpu_sycl/syclBFS.h
+++ include/gpu_sycl/syclBFS.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <CL/sycl.hpp>
 // #include <dpct/dpct.hpp>
-#include <syclcompat/syclcompat.hpp>
+// #include <syclcompat/syclcompat.hpp>
 #include "../csr/csr.h"
 
 struct prescan_result
